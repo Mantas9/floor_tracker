5e9d31bb34d197fbc370920eb50f47e5631e0bf8
Mantas9 <dev.mantasgr@gmail.com>
2025-06-23 18:22:33 +0300

    Export arguments, broken output

--- filesworker/filesworker.go
+++ filesworker/filesworker.go
@@ -8,11 +8,9 @@ import (
 
 // / Result structs
 type Stats struct {
-	Symbol      string
-	FloorPrice  float64
-	ListedCount float64
-
-	RawData []byte
+	Symbol      string  `csv:"symbol" json:"symbol"`
+	FloorPrice  float64 `csv:"floorPrice" json:"floorPrice"`
+	ListedCount float64 `csv:"listedCount" json:"listedCount"`
 }
 
 func (s *Stats) UnmarshalJSON(data []byte) error {
@@ -29,14 +27,16 @@ func (s *Stats) UnmarshalJSON(data []byte) error {
 	s.Symbol = statsData["symbol"].(string)
 	s.FloorPrice = statsData["floorPrice"].(float64) * constants.LamportsToSol
 	s.ListedCount = statsData["listedCount"].(float64)
-	s.RawData = data // original raw data
 
 	return nil
 }
 
-func (s *Stats) WriteToJSON() error {
-	// Write to JSON
-	err := os.WriteFile("./data.json", s.RawData, 0644)
+func (s *Stats) MarshalJSON() ([]byte, error) {
+	return json.Marshal(s)
+}
+
+func WriteJSON(data []byte) error {
+	err := os.WriteFile("./stats.json", data, 0644) // Write string data to file
 
 	return err
 }
--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
 	"sol/floortracker/datafetcher"
 	"sol/floortracker/filesworker"
 	"sync"
+
+	"github.com/gocarina/gocsv"
 )
 
 const ( // Export As variables
@@ -18,68 +21,122 @@ func main() {
 	// Get OS arguments (skip run program variable)
 	args := os.Args[1:]
 
-	// Default export variable
-	export := Terminal
+	// Export variable
+	export := -1
+
+	// Get export option if specified. Else, default to terminal output
+	if args[0][0] == '-' {
+		params := args[0] // Parameters string
+
+		for i := 1; i < len(params); i++ { // Go through each letter argument
+			if export == -1 && (params[i] == 't' || params[i] == 'T') { // Terminal output
+				export = Terminal
+			}
+			if export == -1 && (params[i] == 'j' || params[i] == 'J') { // JSON output
+				export = JSON
+			}
+			if export == -1 && (params[i] == 'c' || params[i] == 'C') { // CSV output
+				export = CSV
+			}
+		}
+
+		// Truncate parameters
+		args = args[1:]
+	} else { // Default to terminal output
+		export = Terminal
+	}
 
 	var wg sync.WaitGroup // Create waitgroup to let code finish before exiting
 
-	// Cycle through the designated arguments and call getStats
+	// Fetch all requested data
+	var data []filesworker.Stats       // Empty list of stats
+	ch := make(chan filesworker.Stats) // Channel for concurrent communication
+
 	for i := 0; i < len(args); i++ {
+		// Call getStats
+		go getStats(args[i], ch)
+
+		// Append new stats to list
+		data = append(data, <-ch)
+	}
+
+	// Onde data list is filled, time to export accordingly
+	switch export {
+	case Terminal: // Prints each collection's stats to terminal
+
+		// Iterate through data
+		for i := 0; i < len(data); i++ {
+			stats := data[i] // Idividual stats variable of single collection
+
+			// Print to terminal
+			fmt.Println("==============================")
+			fmt.Println("Collection Name:", stats.Symbol)
+			fmt.Println("Floor Price:", stats.FloorPrice, "SOL")
+			fmt.Println("Listed Count:", stats.ListedCount)
+			fmt.Println("==============================")
+			fmt.Println() // Space
+		}
+
+	case JSON:
+
+		// Marshal data to JSON
+		jsonData, err := json.Marshal(data)
+
+		if err != nil { // Error check
+			panic(err)
+		}
+
+		// Write JSON to file
 		wg.Add(1)
 		go func() {
 			defer wg.Done()
-			exportStats(args[i], export)
-		}
+
+			filesworker.WriteJSON(jsonData)
+		}()
+
+	case CSV:
+
+		// Waitgroup
+		wg.Add(1)
+
+		// Concurrent writing
+		go func() {
+			defer wg.Done()
+
+			// Create result CSV file
+			file, err := os.Create("stats.csv")
+			if err != nil {
+				panic(err)
+			}
+
+			// Marshal data struct list to CSV and write to file
+			if err := gocsv.MarshalFile(&data, file); err != nil {
+				panic(err)
+			}
+		}()
+
 	}
 
 	wg.Wait()
 }
 
-// Gets stats and exports them according to user's choice
-func exportStats(arg string, exportAs int) {
-	// Create channel to block for getStats
-	ch := make(chan filesworker.Stats)
-
-	// Get stats from argument
-	go getStats(arg, ch)
-
-	// Channel data to variable
-	stats := <-ch
-
-	// ExportAs logic
-	switch exportAs {
-	case Terminal: // Simply print the values to terminal
-		fmt.Println("==============================")
-		fmt.Println("Collection Name:", stats.Symbol)
-		fmt.Println("Floor Price:", stats.FloorPrice)
-		fmt.Println("Listed Count:", stats.ListedCount)
-		fmt.Println("==============================")
-
-	case JSON: // Write RAW data to a JSON file
-		err := stats.WriteToJSON()
-
-		// Panic if something goes wrong
-		if err != nil {
-			panic(err)
-		}
-	}
-}
-
-// Returns NFT stats in the corresponding struct (declared in jsonworker module)
+// Returns NFT stats in the corresponding struct format
 func getStats(symbol string, rc chan filesworker.Stats) {
 	// Make a channel for communicating with HTTP data
 	ch := make(chan []byte)
 
-	// Run goroutine to get online data
-	go datafetcher.GetStats(symbol, ch)
+	// Get online data
+	datafetcher.GetStats(symbol, ch)
 
 	// Block until datafetcher finishes and dump data to rawData variable
 	rawData := <-ch
 
 	var stats filesworker.Stats // Result variable
 
-	// Fill with designated values
-	stats.UnmarshalJSON(rawData)
+	err := stats.UnmarshalJSON(rawData)
+	if err != nil {
+		panic(err)
+	}
 
 	rc <- stats
 }
